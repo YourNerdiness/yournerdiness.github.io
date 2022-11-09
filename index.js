5c32a8e4988bd490837cfe093e0fd3f98a179ea0
YourNerdiness <91400199+YourNerdiness@users.noreply.github.com>
2022-11-09 15:25:17 +1100

    first release

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+
+       $("select").select2();
+       
+    });
+
 const fileList = ["ad.png", "ae.png", "af.png", "ag.png", "ai.png", "al.png", "am.png", "ao.png", "aq.png", "ar.png", "as.png", "at.png", "au.png", "aw.png", "ax.png", "az.png", "ba.png", "bb.png", "bd.png", "be.png", "bf.png", "bg.png", "bh.png", "bi.png", "bj.png", "bl.png", "bm.png", "bn.png", "bo.png", "bq.png", "br.png", "bs.png", "bt.png", "bv.png", "bw.png", "by.png", "bz.png", "ca.png", "cc.png", "cd.png", "cf.png", "cg.png", "ch.png", "ci.png", "ck.png", "cl.png", "cm.png", "cn.png", "co.png", "cr.png", "cu.png", "cv.png", "cw.png", "cx.png", "cy.png", "cz.png", "de.png", "dj.png", "dk.png", "dm.png", "do.png", "dz.png", "ec.png", "ee.png", "eg.png", "eh.png", "er.png", "es.png", "et.png", "fi.png", "fj.png", "fk.png", "fm.png", "fo.png", "fr.png", "ga.png", "gb-eng.png", "gb-nir.png", "gb-sct.png", "gb-wls.png", "gb.png", "gd.png", "ge.png", "gf.png", "gg.png", "gh.png", "gi.png", "gl.png", "gm.png", "gn.png", "gp.png", "gq.png", "gr.png", "gs.png", "gt.png", "gu.png", "gw.png", "gy.png", "hk.png", "hm.png", "hn.png", "hr.png", "ht.png", "hu.png", "id.png", "ie.png", "il.png", "im.png", "in.png", "io.png", "iq.png", "ir.png", "is.png", "it.png", "je.png", "jm.png", "jo.png", "jp.png", "ke.png", "kg.png", "kh.png", "ki.png", "km.png", "kn.png", "kp.png", "kr.png", "kw.png", "ky.png", "kz.png", "la.png", "lb.png", "lc.png", "li.png", "lk.png", "lr.png", "ls.png", "lt.png", "lu.png", "lv.png", "ly.png", "ma.png", "mc.png", "md.png", "me.png", "mf.png", "mg.png", "mh.png", "mk.png", "ml.png", "mm.png", "mn.png", "mo.png", "mp.png", "mq.png", "mr.png", "ms.png", "mt.png", "mu.png", "mv.png", "mw.png", "mx.png", "my.png", "mz.png", "na.png", "nc.png", "ne.png", "nf.png", "ng.png", "ni.png", "nl.png", "no.png", "np.png", "nr.png", "nu.png", "nz.png", "om.png", "pa.png", "pe.png", "pf.png", "pg.png", "ph.png", "pk.png", "pl.png", "pm.png", "pn.png", "pr.png", "ps.png", "pt.png", "pw.png", "py.png", "qa.png", "re.png", "ro.png", "rs.png", "ru.png", "rw.png", "sa.png", "sb.png", "sc.png", "sd.png", "se.png", "sg.png", "sh.png", "si.png", "sj.png", "sk.png", "sl.png", "sm.png", "sn.png", "so.png", "sr.png", "ss.png", "st.png", "sv.png", "sx.png", "sy.png", "sz.png", "tc.png", "td.png", "tf.png", "tg.png", "th.png", "tj.png", "tk.png", "tl.png", "tm.png", "tn.png", "to.png", "tr.png", "tt.png", "tv.png", "tw.png", "tz.png", "ua.png", "ug.png", "um.png", "us.png", "uy.png", "uz.png", "va.png", "vc.png", "ve.png", "vg.png", "vi.png", "vn.png", "vu.png", "wf.png", "ws.png", "xk.png", "ye.png", "yt.png", "za.png", "zm.png", "zw.png"]
 
 const countryCodeToName = {
@@ -41,6 +47,7 @@ const countryCodeToName = {
     "CM" : "Cameroon",
     "CA" : "Canada",
     "CV" : "Cape Verde",
+    "BQ" : "Caribbean Netherlands",
     "KY" : "Cayman Islands",
     "CF" : "Central African Republic",
     "TD" : "Chad",
@@ -54,9 +61,10 @@ const countryCodeToName = {
     "CD" : "Congo, Democratic Republic",
     "CK" : "Cook Islands",
     "CR" : "Costa Rica",
-    "CI" : "Cote D\"Ivoire",
+    "CI" : "Cote D\'Ivoire",
     "HR" : "Croatia",
     "CU" : "Cuba",
+    "CW" : "Curaçao",
     "CY" : "Cyprus",
     "CZ" : "Czech Republic",
     "DK" : "Denmark",
@@ -66,6 +74,7 @@ const countryCodeToName = {
     "EC" : "Ecuador",
     "EG" : "Egypt",
     "SV" : "El Salvador",
+    "GB-ENG" : "England",
     "GQ" : "Equatorial Guinea",
     "ER" : "Eritrea",
     "EE" : "Estonia",
@@ -117,9 +126,10 @@ const countryCodeToName = {
     "KE" : "Kenya",
     "KI" : "Kiribati",
     "KR" : "Korea",
+    "XK" : "Kosovo",
     "KW" : "Kuwait",
     "KG" : "Kyrgyzstan",
-    "LA" : "Lao People\"s Democratic Republic",
+    "LA" : "Lao People\'s Democratic Republic",
     "LV" : "Latvia",
     "LB" : "Lebanon",
     "LS" : "Lesotho",
@@ -163,7 +173,9 @@ const countryCodeToName = {
     "NG" : "Nigeria",
     "NU" : "Niue",
     "NF" : "Norfolk Island",
+    "GB-NIR" : "Northern Ireland",
     "MP" : "Northern Mariana Islands",
+    "KP" : "North Korea",
     "NO" : "Norway",
     "OM" : "Oman",
     "PK" : "Pakistan",
@@ -199,12 +211,14 @@ const countryCodeToName = {
     "SC" : "Seychelles",
     "SL" : "Sierra Leone",
     "SG" : "Singapore",
+    "SX" : "Sint Maarten",
     "SK" : "Slovakia",
     "SI" : "Slovenia",
     "SB" : "Solomon Islands",
     "SO" : "Somalia",
     "ZA" : "South Africa",
     "GS" : "South Georgia And Sandwich Isl.",
+    "SS" : "South Sudan",
     "ES" : "Spain",
     "LK" : "Sri Lanka",
     "SD" : "Sudan",
@@ -241,9 +255,139 @@ const countryCodeToName = {
     "VN" : "Viet Nam",
     "VG" : "Virgin Islands, British",
     "VI" : "Virgin Islands, U.S.",
+    "GB-WLS" : "Whales",
     "WF" : "Wallis And Futuna",
     "EH" : "Western Sahara",
     "YE" : "Yemen",
     "ZM" : "Zambia",
     "ZW" : "Zimbabwe"
 };
+
+const countries = Object.values(countryCodeToName);
+
+let country = "";
+let current = 0;
+
+function init() {
+
+    let guessFields = document.getElementsByClassName("guess");
+
+    for (let i = 0; i < guessFields.length; i++) {
+
+        for (let j = 0; j < countries.length; j++) {
+
+            let option = document.createElement("option");
+
+            option.textContent = countries[j];
+            option.value = countries[j];
+
+            guessFields[i].appendChild(option);
+
+        }
+
+    }
+
+    next();
+
+}
+
+function guess() {
+
+    if (document.getElementById("g" +  current).value == country) {
+
+        document.getElementById("output").textContent = "Correct!!!"
+
+        document.getElementById("next").disabled = false;
+        document.getElementById("guess").disabled = true;
+
+    }
+
+    else {
+
+        current++;
+
+        shiftGuessFields();
+
+    }
+
+    if (current > 5) {
+
+        document.getElementById("output").textContent = country
+
+        document.getElementById("next").disabled = false;
+        document.getElementById("guess").disabled = true;
+
+    }
+
+}
+
+function shiftGuessFields() {
+
+    let guessFields = document.getElementsByClassName("guess");
+
+    for (let i = 0; i < guessFields.length; i++) {
+
+        if (i == current) {
+
+            guessFields[i].disabled = false;
+
+        }
+
+        else {
+
+            guessFields[i].disabled = true;
+
+        }
+
+    }
+
+}
+
+function next() {
+
+    document.getElementById("next").disabled = true;
+    document.getElementById("guess").disabled = false;
+
+    document.getElementById("output").textContent = ""
+
+    current = 0;
+
+    shiftGuessFields();
+
+    let guessFields = document.getElementsByClassName("guess");
+
+    for (let i = 0; i < guessFields.length; i++) {
+
+        guessFields[i].value = "";
+
+    }
+
+    displayImage();
+
+}
+
+function displayImage() {
+
+    let image = document.getElementById("flag");
+
+    let pictureName = fileList[randNumber(0, fileList.length)].slice(0, -4);
+
+    let fileName = "./flags/" + pictureName + ".png";
+    
+    image.src = fileName;
+
+    country = countryCodeToName[pictureName.toUpperCase()];
+
+    if (country == undefined) {
+
+        alert("Hi. My code has detected an error. If you could please report an issue here (https://github.com/YourNerdiness/yournerdiness.github.io/issues), with the error code 1000" + pictureName.toUpperCase() + ", I would greatly appreciate it");
+
+    }
+
+}
+
+function randNumber(lowerbound, upperBound) {
+
+    return lowerbound + Math.floor(Math.random()*upperBound);
+
+}
