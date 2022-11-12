e8d4e7ba66cee533f53cca92b21d83256642a080
YourNerdiness <91400199+YourNerdiness@users.noreply.github.com>
2022-11-12 13:49:20 +1100

    second release

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-    $('select').selectize({
-        sortField: 'text'
-    });
-});
-
 const fileList = ["ad.png", "ae.png", "af.png", "ag.png", "ai.png", "al.png", "am.png", "ao.png", "aq.png", "ar.png", "as.png", "at.png", "au.png", "aw.png", "ax.png", "az.png", "ba.png", "bb.png", "bd.png", "be.png", "bf.png", "bg.png", "bh.png", "bi.png", "bj.png", "bl.png", "bm.png", "bn.png", "bo.png", "bq.png", "br.png", "bs.png", "bt.png", "bv.png", "bw.png", "by.png", "bz.png", "ca.png", "cc.png", "cd.png", "cf.png", "cg.png", "ch.png", "ci.png", "ck.png", "cl.png", "cm.png", "cn.png", "co.png", "cr.png", "cu.png", "cv.png", "cw.png", "cx.png", "cy.png", "cz.png", "de.png", "dj.png", "dk.png", "dm.png", "do.png", "dz.png", "ec.png", "ee.png", "eg.png", "eh.png", "er.png", "es.png", "et.png", "fi.png", "fj.png", "fk.png", "fm.png", "fo.png", "fr.png", "ga.png", "gb-eng.png", "gb-nir.png", "gb-sct.png", "gb-wls.png", "gb.png", "gd.png", "ge.png", "gf.png", "gg.png", "gh.png", "gi.png", "gl.png", "gm.png", "gn.png", "gp.png", "gq.png", "gr.png", "gs.png", "gt.png", "gu.png", "gw.png", "gy.png", "hk.png", "hm.png", "hn.png", "hr.png", "ht.png", "hu.png", "id.png", "ie.png", "il.png", "im.png", "in.png", "io.png", "iq.png", "ir.png", "is.png", "it.png", "je.png", "jm.png", "jo.png", "jp.png", "ke.png", "kg.png", "kh.png", "ki.png", "km.png", "kn.png", "kp.png", "kr.png", "kw.png", "ky.png", "kz.png", "la.png", "lb.png", "lc.png", "li.png", "lk.png", "lr.png", "ls.png", "lt.png", "lu.png", "lv.png", "ly.png", "ma.png", "mc.png", "md.png", "me.png", "mf.png", "mg.png", "mh.png", "mk.png", "ml.png", "mm.png", "mn.png", "mo.png", "mp.png", "mq.png", "mr.png", "ms.png", "mt.png", "mu.png", "mv.png", "mw.png", "mx.png", "my.png", "mz.png", "na.png", "nc.png", "ne.png", "nf.png", "ng.png", "ni.png", "nl.png", "no.png", "np.png", "nr.png", "nu.png", "nz.png", "om.png", "pa.png", "pe.png", "pf.png", "pg.png", "ph.png", "pk.png", "pl.png", "pm.png", "pn.png", "pr.png", "ps.png", "pt.png", "pw.png", "py.png", "qa.png", "re.png", "ro.png", "rs.png", "ru.png", "rw.png", "sa.png", "sb.png", "sc.png", "sd.png", "se.png", "sg.png", "sh.png", "si.png", "sj.png", "sk.png", "sl.png", "sm.png", "sn.png", "so.png", "sr.png", "ss.png", "st.png", "sv.png", "sx.png", "sy.png", "sz.png", "tc.png", "td.png", "tf.png", "tg.png", "th.png", "tj.png", "tk.png", "tl.png", "tm.png", "tn.png", "to.png", "tr.png", "tt.png", "tv.png", "tw.png", "tz.png", "ua.png", "ug.png", "um.png", "us.png", "uy.png", "uz.png", "va.png", "vc.png", "ve.png", "vg.png", "vi.png", "vn.png", "vu.png", "wf.png", "ws.png", "xk.png", "ye.png", "yt.png", "za.png", "zm.png", "zw.png"]
 
 const countryCodeToName = {
@@ -271,9 +265,11 @@ let current = 0;
 
 function init() {
 
-    let guessFields = document.getElementsByClassName("guess");
+    let guessFields = document.getElementsByClassName("guessS");
 
-    for (let i = 0; i < guessFields.length; i++) {
+    for (let i = 0; i < 6; i++) {
+
+        let guessField = guessFields[i];
 
         for (let j = 0; j < countries.length; j++) {
 
@@ -282,7 +278,7 @@ function init() {
             option.textContent = countries[j];
             option.value = countries[j];
 
-            guessFields[i].appendChild(option);
+            guessField.appendChild(option);
 
         }
 
@@ -292,11 +288,70 @@ function init() {
 
 }
 
+function stringDifference(str1, str2) {
+
+    if (str1.length == 0) return str2.length;
+    if (str2.length == 0) return str1.length;
+
+    let distance = 0;
+
+    if (str1.length < str2.length) {
+
+        distance = str2.length - str1.length;
+
+        for (let i = 0; i < str1.length; i++) {
+
+            if (str1[i] != str2[i]) distance++;
+            
+        }
+
+    }
+
+    else {
+
+        distance = str1.length - str2.length;
+
+        for (let i = 0; i < str2.length; i++) {
+
+            if (str1[i] != str2[i]) distance++;
+            
+        }
+
+    }
+
+    return distance;
+
+}
+
+function searchForGuess() {
+
+    let currentGuess = document.getElementById("g" + current).value;
+
+    let minMatch = 1000;
+    let selectedCountry = "";
+
+    for (let i = 0; i < countries.length; i++) {
+
+        closeness = stringDifference(countries[i], currentGuess);
+
+        if (closeness < minMatch) {
+
+            minMatch = closeness;
+            selectedCountry = countries[i];
+
+        }
+
+        if (minMatch == 0) break;
+
+    }
+
+    document.getElementById("s" + current).value = selectedCountry;
+
+}
+
 function guess() {
 
-    console.log(country);
-
-    if (document.getElementById("g" +  current).value == country) {
+    if (document.getElementById("s" +  current).value == country) {
 
         document.getElementById("output").textContent = "Correct!!!"
 
@@ -306,6 +361,8 @@ function guess() {
     }
 
     else {
+
+        document.getElementById("output").textContent = "Incorrect";
 
         current++;
 
@@ -326,19 +383,25 @@ function guess() {
 
 function shiftGuessFields() {
 
-    let guessFields = document.getElementsByClassName("guess");
+    let guessFieldsG = document.getElementsByClassName("guessG");
+    let guessFieldsS = document.getElementsByClassName("guessS");
 
-    for (let i = 0; i < guessFields.length; i++) {
+    for (let i = 0; i < 6; i++) {
+
+        let guessFieldG = guessFieldsG[i];
+        let guessFieldS = guessFieldsS[i];
 
         if (i == current) {
 
-            guessFields[i].disabled = false;
+            guessFieldG.disabled = false;
+            guessFieldS.disabled = false;
 
         }
 
         else {
 
-            guessFields[i].disabled = true;
+            guessFieldG.disabled = true;
+            guessFieldS.disabled = true;
 
         }
 
@@ -379,7 +442,7 @@ function displayImage() {
 
     if (country == undefined) {
 
-        alert("Hi. My code has detected an error. If you could please report an issue here (https://github.com/YourNerdiness/yournerdiness.github.io/issues), with the error code 1000" + pictureName.toUpperCase() + ", I would greatly appreciate it");
+        alert("Hi. My code has detected an error. If you could please report an issue here (https://github.com/YourNerdiness/yournerdiness.github.io/issues), with the error code 1000" + pictureName.toUpperCase() + ", I would greatly appreciate it. To continue playing, reload the page.");
 
     }
 
