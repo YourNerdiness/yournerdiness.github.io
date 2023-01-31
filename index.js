c4d1ada43be6c885b65422283a0cf3971f0a1801
YourNerdiness <91400199+YourNerdiness@users.noreply.github.com>
2023-01-31 14:34:52 +1100

    actually fixed bug in stringDifference function

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ function stringDifference(str1, str2) {
 
     console.log(str1, str2, ((str2 || "").match(new RegExp("[" + str1 + "]", "gi")) || []).length);
 
-    return ((str2 || "").match(new RegExp(str1, "giu")) || []).length;
+    return ((str2 || "").match(new RegExp("[" + str1 + "]", "giu")) || []).length;
 
 }
 
